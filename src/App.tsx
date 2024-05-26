5d8db47bb50460bd7b91bf7e5209c7b0dca8005a
Aditya Garg <adityagarg@Adityas-MacBook-Air.local>
2024-05-26 15:24:27 +0530

    route fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Home from './Pages/Home/Home.page';
 import Header from './Pages/Header';
-import { lazy } from 'react';
-import {  Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import DrSanjeetRana from './Pages/DrSanjeetRana/DrSanjeetRana.component';
-
-const Plans = lazy(() => import('./Pages/Plans'));
-const Footer = lazy(() => import('./Pages/Footer'));
-const Reviews = lazy(() => import('./Pages/Reviews'));
-const Features = lazy(() => import('./Pages/Features'));
+import Features from './Pages/Features';
+import Reviews from './Pages/Reviews';
+import Plans from './Pages/Plans';
+import Footer from './Pages/Footer';
 
 const App = () => {
   return (
@@ -24,12 +22,12 @@ const App = () => {
               <Features />
               <Reviews />
               <Plans />
-              <Footer />
             </div>
           }
         />
         <Route path='/drsanjeetrana' element={<DrSanjeetRana />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
