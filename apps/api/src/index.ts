7ca1aefc6ee45b4987cf21215dbf6af9bba06e5c
Aditya Garg <adityagarg@Adityas-MacBook-Air.local>
2025-11-05 23:25:54 +0530

    port added

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,6 +3,7 @@ import Fastify from "fastify";
 const fastify = Fastify({
   logger: true,
 });
+const port = Number(process.env.PORT) || 3000;
 
 fastify.get("/", async (_request, reply) => {
   reply.send({ hello: "world" });
@@ -10,8 +11,8 @@ fastify.get("/", async (_request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
-    console.log("Server listening on http://localhost:3000");
+    await fastify.listen({ port, host: "0.0.0.0" });
+    console.log("Server listening on", port);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
